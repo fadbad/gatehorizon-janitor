b18ec2edbbcd001c8c04ef5ceb3409f6ada98be4
fadi Badawi <ifadbad@gmail.com>
2022-01-18 19:20:32 +0400

    ---

--- src/Config.js
+++ src/Config.js
@@ -7,7 +7,6 @@ export default {
     BASE: `${BASE}`,
     API: `${BASE}/api/janitor`,
     ONESIGNAL: '2bae8d7a-d3cd-4960-a943-637d9da57b26',
-    // GOOGLE_MAPS: 'AIzaSyADaG_W7_vwxxVJn2xunQkIRQwoA6YQF3g',
 	GOOGLE_MAPS: 'AIzaSyDyDmDW0IC74CE6vayHwQDAWXpZgko7YTE',
     APPLE_APPID: '1604267369',
 	GooglePackageName: 'com.gatehorizon.janitor',
